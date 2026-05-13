8f3cf50946ba1ac2b3e3e57ed9c94e13e0e19539
Sachin Katiyar <sachin895393@gmail.com>
2026-05-13 11:04:20 -0400

    Updated Backend URL 12-05-2026

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 // const BASE_URL = 'http://127.0.0.1:8000/api';
 
 // 🌐 PRODUCTION BACKEND URL
-const BASE_URL = 'http://80.225.205.208:8000/api';
+const BASE_URL = 'https://notessnipt.duckdns.org/api';
 
 // ============================================================
 // 1. OPEN INSTANCE (For Login, Signup - No Token Required)
